5886622606ab6eb821b73ad8b1ff3aa2839f9281
DrozdMargo <DrozdMargo@i.ua>
2017-03-04 17:40:29 +0200

    create search by hashtag and autorize with bearer token

---
+++ beckend/config.js
@@ -0,0 +1,7 @@
+var appsettings = {
+  consumerkey: '1E04M7YyXM3QRE2MbTb4t1fYZ',
+  consumersecret: 'gWNMzAnZUhhXhnMwRpAcHowb2nrGTeRofZ2urgQHqKAWwH56c6',
+  bearertoken: ''
+};
+
+module.exports = appsettings;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { Http, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
+  searchquery = '';
+  tweetsdata;
+
+  constructor(private http: Http) {
+  }
+
+  makeCall() {
+    var headers = new Headers();
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this.http.post('http://localhost:3000/authorize', {headers: headers}).subscribe((res) => {
+      console.log(res);
+    })
+  }
+
+  searchCall() {
+    var headers = new Headers();
+    var searchterm = 'query=' + this.searchquery;
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this.http.post('http://localhost:3000/search', searchterm, {headers: headers}).subscribe((res) => {
+      this.tweetsdata = res.json().data.statuses;
+    });
+  }
+
+  usercall(){
+    var headers = new Headers();
+    var searchterm = 'screenname=Yasmin_Payne1';
+
+    headers.append('Content-Type', 'application/X-www-form-urlencoded');
+
+    this.http.post('http://localhost:3000/user', searchterm, {headers: headers}).subscribe((res) => {
+      console.log(res.json().data);
+
+    });
+  }
 }
